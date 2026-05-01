204dae96ac2e870162877b5b76fa4c4cca411a33
Loana Muñoz <u22314058@utp.edu.pe>
2026-05-01 13:57:03 -0500

    feat: implementa CRUD de Usuario (entity, repository, service, controller) (Closes #5)

---
+++ src/main/java/com/Eugeo/gestion_restaurante/Controller/UsuarioController.java
@@ -0,0 +1,28 @@
+package com.Eugeo.gestion_restaurante.Controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Eugeo.gestion_restaurante.Entity.Usuario;
+import com.Eugeo.gestion_restaurante.Service.UsuarioService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/usuarios")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
+public class UsuarioController {
+
+    private final UsuarioService service;
+
+    @GetMapping
+    public List<Usuario> findAll() {
+        return service.findAll();
+    }
+
+}
